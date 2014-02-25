3f9a5deb9b80bf99c6178d98021e4532f8ebd286
seb <seb@seb.fr>
2014-02-25 14:02:35 +0100

    Ajout d'agent

---
+++ src/fr/univ/m2m/core/Incoming.java
@@ -0,0 +1,49 @@
+package fr.univ.savoie.multiagent.core;
+
+import java.net.*;
+import java.io.*;
+ 
+/**
+ * The Incoming class corresponds to the server socket
+ *
+ */
+public class Incoming extends Thread {
+	
+	//the server socket port
+	int port;
+	
+	/**
+	 * Constructor sets the port for the server socket
+	 * @param port the server socket port
+	 */
+	public Incoming(String port) {
+		
+		this.port = Integer.parseInt(port);
+		
+	} //constructor
+	
+	/**
+	 * the run method waits for socket connections to initiate a thread
+	 * for this specific communication
+	 */
+    public void run() {
+ 
+    	//ensure we are waiting for any new connections
+        boolean listening = true;
+         
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        	
+            while (listening) 
+                new IncomingThread(serverSocket.accept()).start();
+            
+        } //try
+        catch (IOException e) {
+
+        	System.err.println("Could not listen on port " + port);
+            System.exit(-1);
+            
+        } //catch
+        
+    } //run()
+    
+} //class Incoming
---
+++ src/fr/univ/m2m/core/IncomingThread.java
@@ -0,0 +1,88 @@
+package fr.univ.savoie.multiagent.core;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * The IncomingThread corresponds to the protocol used by the agent
+ *
+ */
+public class IncomingThread extends Thread {
+	
+	//the client socket
+    private Socket socket = null;
+ 
+    /**
+     * Constructor set the client socket
+     * @param socket the client socket
+     */
+    public IncomingThread(Socket socket) {
+    	
+        super("IncomingThread");
+        this.socket = socket;
+        
+    } //constructor
+    
+    /**
+     * The run method  
+     */
+    public void run() {
+ 
+        try {
+        
+        	//create the incoming and the outgoing streams
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()));
+        
+            //contains the incoming message
+            String inputLine;
+            
+            //receiving the connection from the client, the server sends back
+            //Start
+            out.println("Start");
+ 
+            //while the client continues to send messages
+            while ((inputLine = in.readLine()) != null) {
+            	
+            	//what is received
+                System.out.println("From client: " + inputLine);
+
+                //the protocol, here the Contract Net
+                if (inputLine.equals("cfp")) {
+                	
+                	out.println("propose");
+                	
+                } //if
+                
+                if (inputLine.equals("accept-proposal")) {
+                	
+                	out.println("inform-done");
+                	
+                } //if
+                
+                if (inputLine.equals("reject-proposal")) {
+                	
+                } //if
+                
+            } //while
+            
+            //close the streams
+            out.close();
+            in.close();
+            socket.close();
+            
+        } //try
+        catch (IOException e) {
+        
+        	e.printStackTrace();
+        	
+        } //catch
+        
+    } //run()
+    
+} //class IncomingThread
